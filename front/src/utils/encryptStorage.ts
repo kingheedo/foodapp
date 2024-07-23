8ab3674c2bafd67ad103f05caf6aa949f94ba567
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-07-23 21:13:06 +0900

    feat(EncryptStorage): 설치 및 util 설정

---
+++ front/src/utils/encryptStorage.ts
@@ -0,0 +1,33 @@
+import EncryptedStorage from 'react-native-encrypted-storage/lib/typescript/EncryptedStorage';
+
+const setEncryptedStorage = async <T>(key: string, data: T) => {
+  try {
+    await EncryptedStorage.setItem(key, JSON.stringify(data));
+  } catch (error) {
+    console.log('setEncryptedStorage error', error);
+  }
+};
+
+const getEncryptedStorage = async (key: string) => {
+  try {
+    const data = await EncryptedStorage.getItem(key);
+
+    return data ? JSON.parse(data) : null;
+  } catch (error) {
+    console.log('getEncryptedStorage error', error);
+  }
+};
+
+const removeEncryptedStorage = async (key: string) => {
+  try {
+    const data = await getEncryptedStorage(key);
+
+    if (data) {
+      await EncryptedStorage.removeItem(key);
+    }
+  } catch (error) {
+    console.log('removeEncryptedStorage error', error);
+  }
+};
+
+export {setEncryptedStorage, getEncryptedStorage, removeEncryptedStorage};
